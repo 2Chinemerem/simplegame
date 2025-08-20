6dce482c7097d81b179f389745de058733b8d64a
Onwubualili Chinemerem <chinemerememma007@gmail.com>
2025-08-21 00:34:07 +0100

    cool functionality added

--- GAME/script.js
+++ GAME/script.js
@@ -1,173 +1,229 @@
+var playerOne = "Player 1"
+var playerTwo= "Player 2"
 
-var playerOne = prompt("Player One: Enter your name, you will be X")
-var playerTwo = prompt("Player Two: Enter your name, you will be Y")
+var currentPlayer = -1;
+var currentPlayerName= ""
+var table = document.querySelectorAll("table tr");
+var button = document.querySelectorAll("td button");
+var restartbutton = document.querySelector(".restartbutton");
 
-var table = $('table tr')
-var button = $('button')
-var restartbutton = $('.c')
 
-var squares = $('button')
-
-// Clear Squares Function
 function colorMatchCheck(one,two,three,){
   return (one===two && one===three && one !== ' ' && one !== undefined);
 }
 
-function checkforX(rowindex, colindex){
-    return table.eq(rowindex).find('td').eq(colindex).find('button').text();
+function checkforX(rowIndex, colIndex) {
+  const rows = document.querySelectorAll("table tr");
+  const row = rows[rowIndex];
+  if (!row) return "";
+  
+  const cells = row.querySelectorAll("td");
+  const cell = cells[colIndex];
+  if (!cell) return "";
+
+
+  const button = cell.querySelector("button");
+  return button ? button.textContent : "";
+}
+function reportwin(rowIndex, colIndex, winmethod){
+  console.log(currentPlayerName + " won starting from " + rowIndex+ " "+ colIndex)
+  if (winmethod== "horizontal"){
+    console.log("Horizontal win")
+    button[rowIndex*3+colIndex].style.backgroundColor= 'green'
+    button[rowIndex*3+(colIndex+1)].style.backgroundColor= 'green'
+    button[rowIndex*3+(colIndex+2)].style.backgroundColor= 'green'
+  }
+
+  else if (winmethod== "vertical"){
+    console.log("Vertical win")
+    button[rowIndex*3+colIndex].style.backgroundColor= 'green'
+    button[(rowIndex+1)*3+colIndex].style.backgroundColor= 'green'
+    button[(rowIndex+2)*3+colIndex].style.backgroundColor= 'green'
+  }
+  else if (winmethod== "leftdiagonal"){
+    console.log("Left diagonal win")
+    button[rowIndex*3+colIndex].style.backgroundColor= 'green'
+    button[(rowIndex+1)*3+(colIndex-1)].style.backgroundColor= 'green'
+    button[(rowIndex+2)*3+(colIndex-2)].style.backgroundColor= 'green'
+  }
+  else{
+    console.log("Right diagonal win")
+    button[rowIndex*3+colIndex].style.backgroundColor= 'green'
+    button[(rowIndex+1)*3+(colIndex+1)].style.backgroundColor= 'green'
+    button[(rowIndex+2)*3+(colIndex+2)].style.backgroundColor= 'green'
+  }
 
 }
 
-function reportWin(rowNum,colNum) {
-  console.log("You won starting at this row,col");
-  console.log(rowNum);
-  console.log(colNum);
-}
 
-//begining of the horizontal win check
+//Begining of the horizontal win check
 function horizontalWinCheck() {
   for (var row = 0; row <= 2; row++) {
     for (var col = 0; col <= 2; col++) {
       if (colorMatchCheck(checkforX(row,col), checkforX(row,col+1) ,checkforX(row,col+2))) {
-        console.log('horiz');
-        reportWin(row,col)
-        
+        reportwin(row,col,"horizontal")
         return true;
-      }else {
-        continue;
       }
     }
   }
 }
 // End of the horizontal win check
 
+
+//Begining of the Vertical win check
+function verticalWinCheck() {
+  for (var row = 0; row <= 2; row++) {
+    for (var col = 0; col <= 2; col++) {
+      if (colorMatchCheck(checkforX(row,col), checkforX(row+1,col) ,checkforX(row+2,col))) {
+        console.log('vertical win');
+        reportwin(row,col, "vertical")
+        return true;
+      }
+    }
+  }
+}
+// End of the Vertical win check
+
+
+//Begining of the right diagonal win check
+function rightDiagonalCheck() {
+  for (var row = 0; row <= 2; row++) {
+    for (var col = 0; col <= 2; col++) {
+      if (colorMatchCheck(checkforX(row,col), checkforX(row+1,col+1) ,checkforX(row+2,col+2))) {
+        console.log('Diagonal win');
+        reportwin(row,col, "rightdiagonal")
+        return true;
+      }
+    }
+  }
+}
+// End of the right diagonal win check
+
+
+
+//Begining of the left diagonal win check
+function leftDiagonalCheck() {
+  for (var row = 0; row <= 2; row++) {
+    for (var col = 0; col <= 2; col++) {
+      if (colorMatchCheck(checkforX(row,col), checkforX(row+1,col-1) ,checkforX(row+2,col-2))) {
+        console.log('Left Diagonal win');
+        reportwin(row,col, "leftdiagonal")
+        return true;
+      }
+    }
+  }
+}
+// End of the left diagonal win check
+
+
+
+//Game End
 function gameEnd(winningPlayer) {
-  for (var col = 0; col <= 2; col++) {
-    for (var row = 0; row <= 2; row++) {
-      $('h3').fadeOut('fast');
-      // $('h1').fadeOut('fast');
-      $('.b').text(winningPlayer+" has won! Refresh your browser or click restart to play again!").css("fontSize", "50px")
+      
+      
+      document.querySelector("h1").textContent = winningPlayer + " won!!";
+      document.querySelector("h1").style.fontSize = "40px";
+      document.querySelector("h1").style.color= "green";
+      document.querySelectorAll("td button").forEach(el => {
+        el.disabled= true
+      }) 
+
+      document.querySelectorAll("h3").forEach(el => {
+        el.textContent= "Refresh your browser or click restart to play again!"
+        el.style.fontSize = "20px";
+      });
+
+
+    }
+
+// Loop through and check for empty text
+function hasFreeSpaces(){
+  for (let btn of button) {
+    if (btn.textContent.trim() === "") {
+      return true; // found at least one free space
     }
   }
-}
-
-var currentPlayer = 1;
-var currentName = playerOne;
-
-function clearBoard() {
-  for (var index = 0; index <=8; index++) {
-      squares[index].textContent = ' ';
-      squares[index].removeAttribute('disabled')
-    $('.b').text('Welcome to my Tic Tac Toe game').css("fontSize", "32px")
-    $('h3').fadeIn('fast').text(playerOne+": it is your turn, please play");
-
-  }
-  currentPlayer = 1;
-  mouseclick(currentPlayer)
-}
-
-restartbutton.on('click',clearBoard)
-
-
-
-function mouseclick(playerindex){
-  
-  for (let i = 0; i <=8;  i++) {
-  button[i].addEventListener('click', function(){  
-
  
-
-    if (playerindex === 1){
-      button[i].innerHTML = "<H1 style='color:black; font-size: 40px; '>X</H1>"
-      button[i].setAttribute('disabled', 'disabled');
-    button[i].style = 'background-color', 'white';
-    
-    
-    }
-  
-
-  
-    
-    else{
-      button[i].innerHTML = "<H1 style='color:black; font-size: 40px;'>Y</H1>"
-      button[i].setAttribute('disabled', 'disabled');
-  button[i].style = 'background-color', 'white';
-
-
-
-    }
-    
-    
- })
-
+  return false //no free space
 }
-if (horizontalWinCheck()) {
-    gameEnd(currentName);
 
+// Loop through and check for empty text (alternate method)
+// function hasFreeSpaces() {
+//   let emptyCount = Array.from(button).filter(btn => btn.textContent.trim() === "").length;
+
+//   if (emptyCount > 0) {
+//     return true
+//   }
+//   else{
+//     return false // no free spaces
+//   }
+
+// }
+
+
+//Clicking operations
+function mouseclick(currentPlayer, i){
+  if (currentPlayer == 1){
+    button[i].innerHTML = "<H1 style='color:black; font-size: 40px;'>X</H1>"
+    button[i].setAttribute('disabled', 'disabled');
   }
-  
-}
-
-
-// Mouse Over, mouse out and player X
-for (let index = 0; index <=8; index++) {
-    button[index].addEventListener('mouseover', function(){
-    button[index].style.backgroundColor= 'grey'
-})
-button[index].addEventListener('mouseout', function(){
-button[index].style.backgroundColor= 'white'
-})
-
-}
-
-//The real coding part
-
-
-$('h3').text(playerOne+": it is your turn, please play");
-
-
-
-for (let i = 0; i <=8; i++) {
-button[i].addEventListener('click',function(){
-
-  button[i].innerHTML = "<H1 style='color:black; font-size: 40px;'>X</H1>"
-  button[i].setAttribute('disabled', 'disabled');
-  button[i].style = 'background-color', 'white';
- 
-  
-
-  
-  
-    currentPlayer = currentPlayer * -1 ;
-  
-})
-}
-
-
-
-  button.on('click', function(){
-  
-
-  if (currentPlayer === 1) {
-    currentName = playerOne;
-    $('h3').text(currentName+": it is your turn, please play");
-    mouseclick(currentPlayer)
-    
-
-  }
-
-
   
   else{
-    currentName = playerTwo
-    $('h3').text(currentName+": it is your turn, please play");
-    mouseclick(currentPlayer)
-    
-    
-  
-    
+    button[i].innerHTML = "<H1 style='color:black; font-size: 40px;'>O</H1>"
+    button[i].setAttribute('disabled', 'disabled');
+  }
+
+  if (horizontalWinCheck() | verticalWinCheck() | rightDiagonalCheck()| leftDiagonalCheck()) {
+    gameEnd(currentPlayerName);
   }
   
+}
+
+
+//Game Execution
+document.querySelector('h3').textContent= playerOne+": it is your turn, please play (click a button)";
+currentPlayer = currentPlayer* -1;
+
+
+
+for (let i = 0; i <=8;  i++) {
   
-  
+  button[i].addEventListener('click', function(){  
+
+    if (currentPlayer == 1){
+      currentPlayerName= playerOne;
+      document.querySelector('h3').textContent= playerTwo+": it is your turn, please play (click a button)";
+    }
+    else{
+      currentPlayerName = playerTwo;
+      document.querySelector('h3').textContent= playerOne+": it is your turn, please play (click a button)";
+    }
+    mouseclick(currentPlayer, i);
+    currentPlayer= currentPlayer*-1
+    
+    if(!hasFreeSpaces() && !horizontalWinCheck() && !verticalWinCheck() && !rightDiagonalCheck() && !leftDiagonalCheck()){
+      document.querySelector('h3').textContent= "No winner!";
+
+    }
+
+  })
+}
+
+
+
+//Restart button
+restartbutton.addEventListener('click', function(){
+  button.forEach(el => {
+    el.textContent= " "
+    el.disabled= false
+    el.style.backgroundColor= 'white'
+  })
+
+
+  currentPlayer= 1
+  document.querySelector('h3').removeAttribute("style")
+  document.querySelector('h3').textContent= playerOne+": it is your turn, please play (click a button)";
+  document.querySelector("h1").textContent = "Welcome to my Tic Tac Toe game";
+  document.querySelector("h1").removeAttribute("style")
 })
+
